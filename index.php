aafa6cde511e170406765a20438dfdb0ff0081eb
João Victor Jafet <131908647+Joao54270@users.noreply.github.com>
2024-11-06 00:35:34 +0000

    jogo da velha

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Jogo da Velha</title>
+</head>
+<body>
+    <h1>Jogo da velha</h1>
+    <form action="" method="post">
+        <label for="idJogo"></label>
+        <input type="text">
+        <input type="submit" value="Gravar ID">
+    </form>
+
+    <section>
+        <div class="jogo">
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+        </div>
+        <div class="jogo">
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+        </div>
+        <div class="jogo">
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+            <div class="part"><img src="https://img.freepik.com/vetores-gratis/ilustracao-de-desenho-animado-de-desenho-animado-de-velhinha-desenhada-a-mao_52683-115975.jpg" alt=""></div>
+        </div>
+    </section>
+</body>
+</html>
